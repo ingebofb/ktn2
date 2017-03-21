5b6cf8fbee5932c9fc2bfd845d1e58b1da1ea683
ingebofb <ingebofb@stud.ntnu.no>
2017-03-21 20:09:34 +0100

    Add files via upload

--- MessageParser.py
+++ MessageParser.py
@@ -1,28 +1,39 @@
-
+import json
 
 class MessageParser():
     def __init__(self):
-
         self.possible_responses = {
             'error': self.parse_error,
             'info': self.parse_info,
             'message': self.parse_message,
-            'history': self.parse_history,
+            'history': self.parse_history
+            
+	    # More key:values pairs are needed	
         }
-
     def parse(self, payload):
-        payload = # decode the JSON object
+        payload = json.loads(payload)
 
         if payload['response'] in self.possible_responses:
             return self.possible_responses[payload['response']](payload)
         else:
-            # Response not valid
+            return 'unvalid'
 
     def parse_error(self, payload):
+        errorDict = json.loads(payload)
+        errorString = errorDict['content']
+        return errorString
     
     def parse_info(self, payload):
+        infoDict = json.loads(payload)
+        infoString = infoDict['content']
+        return infoString
 
     def parse_message(self, payload):
+        messageDict = json.loads(payload)
+        messageString = messageDict['content']
+        return messageString
 
     def parse_history(self, payload):
-
+        historyDict = json.loads(payload)
+        historyString = historyDict['content']
+        return historyString
